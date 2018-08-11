82aef79108a7d6b57307a3f170973f4ebc88b18a
Mingjie Jiang <jiang@mingjie.info>
2018-08-11 17:17:36 -0400

    Content script optimization

--- static/js/index.js
+++ static/js/index.js
@@ -1,16 +1,34 @@
 AOS.init({
-    duration: 1000,
-    once: true
+	duration: 1000,
+	once: true
 });
 
-particlesJS.load('particles-js', '/js/particles.config.json', function () {
-    console.log('callback - particles.js config loaded');
+particlesJS.load('particles-js', '/js/particles.config.json', function() {
+	console.log('callback - particles.js config loaded');
 });
 
 /**
  * https://github.com/istvan-ujjmeszaros/bootstrap-autohidingnavbar
  */
-$(".navbar.fixed-top").autoHidingNavbar({
-    "showOnUpscroll": false,
-    "showOnBottom": false
+$('.navbar.fixed-top').autoHidingNavbar({
+	showOnUpscroll: false,
+	showOnBottom: false
 });
+
+var loadDeferredStyles = function() {
+	var addStylesNode = document.getElementById('deferred-styles');
+	var replacement = document.createElement('div');
+	replacement.innerHTML = addStylesNode.textContent;
+	document.body.appendChild(replacement);
+	addStylesNode.parentElement.removeChild(addStylesNode);
+};
+var raf =
+	window.requestAnimationFrame ||
+	window.mozRequestAnimationFrame ||
+	window.webkitRequestAnimationFrame ||
+	window.msRequestAnimationFrame;
+if (raf)
+	raf(function() {
+		window.setTimeout(loadDeferredStyles, 0);
+	});
+else window.addEventListener('load', loadDeferredStyles);
